6e078b62def7f6f9d9f94f4909c2f75708867401
Kevin Dian <kevin.dian@hotmail.com>
2024-02-17 17:52:04 -0500

    fix tabsize

--- lua/dev/init.lua
+++ lua/dev/init.lua
@@ -5,4 +5,11 @@ vim.g.netrw_banner = 0
 
 vim.wo.number = true
 vim.wo.relativenumber = true
-vim.o.expandtab = true
+
+local TAB_WIDTH = 4
+local set = vim.opt
+
+set.tabstop = TAB_WIDTH
+set.shiftwidth = TAB_WIDTH
+set.softtabstop = TAB_WIDTH
+set.expandtab = true
